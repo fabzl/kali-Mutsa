c35b514aaf6e0995eb63e29243085053048b44be
Fabian Andrade <fabian@fabz.tv>
2015-08-22 23:03:43 +0100

    responsive images and picture fill added

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -5,18 +5,21 @@
 // --------------------------------------------- //
 // DEFINE GLOBAL LIBS                            //
 // --------------------------------------------- //
-// Uncomment the line below to expose jQuery as a global object to the usual places
-// window.jQuery = window.$ = require('../../../node_modules/jquery/dist/jquery.js');
 
 
 // force compilation of global libs that don't return a value.
 require("./helpers/log");
 require("./helpers/shims");
 
+require("./libs/skrollr");
+require("./libs/picturefill");
+
 //initialise KM object
 var KM = {};
 
 KM.Config = {
+
+	// s : skrollr.init(),
 
 	init : function () {
 
@@ -26,8 +29,52 @@ KM.Config = {
 		// Example module include
 		KM.UI = require('./modules/UI');
 		KM.UI.init();
+
+		KM.Config.informScroll();
+	},
+
+	createNavigation : function () { 
+
+
+	},
+
+	informScroll : function () { 
+		
+		console.log("informScroll");
+		console.log(window.pageXOffset);// Alias for the window.scrollX
+		console.log(window.pageYOffset );//Alias for the window.scrollY
+		console.log(window.scrollX );//Distance from outer edge of browser the viewport scrolled horizontally
+		console.log(window.scrollY );//Distance from top of the viewport to the top of the scrollbar. Value will vary as you scroll downward.
 	}
+
+
 };
 
 
 KM.Config.init();
+
+
+// ;(function() {
+//     var throttle = function(type, name, obj) {
+//         var obj = obj || window;
+//         var running = false;
+//         var func = function() {
+//             if (running) { return; }
+//             running = true;
+//             requestAnimationFrame(function() {
+//                 obj.dispatchEvent(new CustomEvent(name));
+//                 running = false;
+//             });
+//         };
+//         obj.addEventListener(type, func);
+//     };
+
+//     /* init - you can init any event */
+//     throttle ("scroll", "optimizedScroll");
+// })();
+
+// // handle event
+// window.addEventListener("optimizedScroll", function() {
+//     console.log("Resource conscious scroll callback!");
+// });
+
